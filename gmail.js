6f96b463f47f67d54c90bf26073cab4834d86f91
Carol Chen <carol.chen@shopify.com>
2019-10-17 10:26:52 -0400

    gmail you succ

---
+++ gmail.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    if (window.location.href.indexOf("#inbox") == -1) {
+        return;
+    }
+
+    setInterval(function() {
+        // Hide label pills on emails
+        Array.from(document.getElementsByClassName("av")).forEach(function(labelPill) {
+            labelPill.style.display = 'none'
+        })
+
+        // Hide star button on emails
+        Array.from(document.getElementsByClassName("apU xY")).forEach(function(star) {
+            star.style.display = 'none'
+        })
+
+        // Hide emails from default mailbox
+        document.getElementsByClassName("D E G-atb bP")[0].parentNode.nextSibling.nextSibling.nextSibling.style.display = 'none'
+
+        // Hide top bar for default mailbox
+        Array.from(document.getElementsByClassName("D E G-atb bP")).slice(-1)[0].style.display = 'none'
+    }, 500);
+}
